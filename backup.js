13c5b80934776bfb7aa1536fecda061b5c6da1a9
Danny Cho <dannycho7@gmail.com>
2020-04-19 15:05:46 -0700

    Change from require to JSON.parse - avoids pkg warning/failure about dynamic require

--- backup.js
+++ backup.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 
+const fs = require("fs");
+const path = require("path");
+
 const PRMBackupHandler = require("./PRMBackupHandler.js");
 
-const configPath = process.env.CONFIG_PATH || "./config.json";
-const config = require(configPath);
+const configPath = process.env.PRM_BACKUP_CONFIG_PATH || path.join(__dirname, "./config.json");
+const config = JSON.parse(fs.readFileSync(configPath));
+
 new PRMBackupHandler(config).backup();
