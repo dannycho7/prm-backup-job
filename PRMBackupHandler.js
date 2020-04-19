436d453616d6216e05593f06709fe1714bfcd954
Danny Cho <dannycho7@gmail.com>
2020-04-19 14:50:17 -0700

    V1 - compress + upload + cleanup

--- PRMBackupHandler.js
+++ PRMBackupHandler.js
@@ -1,12 +1,18 @@
-const fs = require("fs");
+const fsPromises = require("fs").promises;
 const path = require("path");
 
+const mkdirp = require("mkdirp");
+const moment = require("moment");
+const rimraf = require("rimraf");
+
 const AdmZip = require("adm-zip");
+const SFTPClient = require("ssh2-sftp-client");
 
 class PRMBackupHandler {
     constructor(config) {
         this.validateConfig(config);
         this.config = config;
+        this.sftp = new SFTPClient('PRMBackupHandler');
     }
 
     validateConfig(config) {
@@ -22,26 +28,60 @@ class PRMBackupHandler {
         }
     }
 
-    async handleFile(filePath) {
-        let zip = new AdmZip();
-        zip.addLocalFile(filePath);
-        zip.writeZip(filePath + ".zip");
+    async exportFile(srcFilePath) {
+        const outputFileName = path.parse(srcFilePath).base;
+        const outputFilePath = path.join(this.config.SFTP_EXPORT_DESTINATION_PATH, outputFileName);
+        console.log(`Attempting to upload ${srcFilePath} and saving to ${outputFilePath}...`);
+
+        await this.sftp.connect({
+            host: this.config.SFTP_IP_ADDR,
+            port: this.config.SFTP_PORT,
+            username: this.config.SFTP_USERNAME,
+            password: this.config.SFTP_PASSWORD,
+        })
+            .then(() => {
+                console.log("Connection success");
+                return this.sftp.fastPut(srcFilePath, outputFilePath);
+            })
+            .then(() => {
+                console.log("Upload success");
+            })
+            .catch(err => {
+                console.error(`Error: ${err.message}`);
+            })
+            .finally(() => {
+                this.sftp.end();
+            });
     }
 
-    backup(onSuccessCB) {
-        fs.readdir(this.config.BACKUP_FILES_PATH, (err, fileNames) => {
-            if (err) {
-                console.error(`Unable to find directory ${process.env.PRM_BACKUP_JOB_PATH}`);
-            } else {
-                return Promise.all(fileNames.map(async (fileName) => {
-                    let filePath = path.join(this.config.BACKUP_FILES_PATH, fileName);
-                    await this.handleFile(filePath);
-                }))
-                    .then((_) => {
-                        onSuccessCB();
-                    });
+    async backup() {
+        try {
+            var fileNames = await fsPromises.readdir(this.config.BACKUP_FILES_PATH);
+        } catch (err) {
+            console.error(err);
+
+        }
+
+        const prmTempFileDir = path.join(this.config.BACKUP_FILES_PATH, ".prm-temp");
+        await mkdirp(prmTempFileDir);
+
+        let zip = new AdmZip();
+        await Promise.all(fileNames.map(async (fileName) => {
+            let filePath = path.join(this.config.BACKUP_FILES_PATH, fileName);
+            let fileStat = await fsPromises.stat(filePath)
+            if (!fileStat.isDirectory()) {
+                zip.addLocalFile(filePath);
             }
-        });
+        }));
+
+        const dateStr = moment(new Date()).format('YYYYMMDD-HHmm');
+        const zipFilePath = path.join(prmTempFileDir, `backup-${dateStr}.zip`);
+        zip.writeZip(zipFilePath);
+
+        await this.exportFile(zipFilePath);
+
+        // cleanup temp directory after backup finishes
+        return new Promise((resolve, reject) => rimraf(prmTempFileDir, resolve));
     }
 };
 
--- backup.js
+++ backup.js
@@ -4,4 +4,4 @@ const PRMBackupHandler = require("./PRMBackupHandler.js");
 
 const configPath = process.env.CONFIG_PATH || "./config.json";
 const config = require(configPath);
-new PRMBackupHandler(config).backup(() => { console.log("Backup successful!"); });
+new PRMBackupHandler(config).backup();
